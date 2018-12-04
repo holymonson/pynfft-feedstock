71a5c3993feed4ce94b196e35a4f888a1566a352
Gregory R. Lee <gregory.lee@cchmc.org>
2018-12-04 16:49:56 -0500

    modernize requirements section

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# have to include the path to nfft.h
-echo "[build_ext]" > setup.cfg
-echo "include-dirs=$PREFIX/include/" >> setup.cfg
 
-python -m pip install --no-deps --ignore-installed .
+export C_INCLUDE_PATH=$PREFIX/include  # required as fftw3.h installed here
+
+# # have to include the path to nfft.h
+# echo "[build_ext]" > setup.cfg
+# echo "include-dirs=$PREFIX/include/" >> setup.cfg
+
+$PYTHON -m pip install . --no-deps --ignore-installed -vvv
